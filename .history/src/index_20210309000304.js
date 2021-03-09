3309ea03fb57e572d323bf84e6355c45d65301b8
Osvaldo Amador Salinas <osvaldodaniel0895@gmail.com>
2021-03-09 00:37:15 -0600

    :memo: Test lib icons
    test lib icons

--- .history/src/index_20210309000304.js
+++
@@ -1,8 +0,0 @@
-import Template from '@templates/Template.js';
-import '@styles/main.css';
-
-console.log('hola')
-(async function App() {
-  const main = null || document.getElementById('main');
-  main.innerHTML = await Template();
-})();
